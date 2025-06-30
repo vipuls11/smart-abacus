16e66e25be24ee1f327bda64bb0b56c853a420f3
Arjoo Vishwakarma <arjoovishwakarma@Arjoos-MacBook-Air.local>
2025-06-30 22:29:59 +0530

    section added in home page

--- src/assets/common/config.js
+++ src/assets/common/config.js
@@ -2,4 +2,6 @@ const imageUrl = "/logo/logo.png"
 const imageIos = "/logo/iso-certified.png"
 const imageBanner = "/img/banner.jpg"
 const imageNotfound = "/img/pagenotfound.png"
-export {imageUrl, imageIos, imageBanner, imageNotfound}
+const imageGame = "/img/game-logo.png"
+const imageGirl = "/img/card_image.png"
+export {imageUrl, imageIos, imageBanner, imageNotfound, imageGame, imageGirl}
